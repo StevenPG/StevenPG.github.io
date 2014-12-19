9ae5ccd87846a79ed0fc529529ca71114c9a482c
Steve <steven.p.gantz@gmail.com>
2014-12-18 20:34:21 -0500

    Updated myfirstwidget project into Projects page
    Put myfirstwidget and image into Projects page under completed
    tutorials.

--- javascripts/projectContentScript.js
+++ javascripts/projectContentScript.js
@@ -3,18 +3,27 @@ function showTutorials()
 {
 	document.getElementById("projContent").innerHTML = 
 	'\
+	<div>\
+		<br>\
+		<a href="https://github.com/StevenPG/Widget-Tutorial" target="_blank"> - My First Widget (Clock)</a>\
+		<hr>\
+		<img src="Myfirstwidget.png" alt="screenshot of widget" style="width:38%;height:320px;">\
+		<br><br><br>\
+	</div>\
+	\
 	<div style="width:50%;float:left;">\
 		<br>\
 		<a href="https://github.com/StevenPG/My-First-App-Continued" target="_blank"> - Android Flashlight Application</a>\
 		<hr>\
-		<img src="flashlight.png" alt="Screenshot of application" style="width:64%;height:300px;">\
+		<img src="flashlight.png" alt="Screenshot of application" style="width:76%;height:320px;">\
 		<br><br><br>\
 	</div>\
+	\
 	<div style="width:50%;float:right;">\
 		<br>\
 		<a href="https://github.com/StevenPG/My-First-App" target="_blank"> - My-First-App (via developer.android.com)</a>\
 		<hr>\
-		<img src="myfirstapp.png" alt="Screenshot of application" style="width:64%;height:300px;">\
+		<img src="myfirstapp.png" alt="Screenshot of application" style="width:76%;height:320px;">\
 		<br><br><br>\
 	</div>\
 	\
